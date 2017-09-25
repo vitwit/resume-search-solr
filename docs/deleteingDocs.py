8bbd547c604b813df83346d60ef506ea85931c33
Harish Allakonda <ahari884@gmail.com>
2017-09-26 03:29:59 +0530

    deletingDocs added

--- docs/deleteingDocs.py
+++ docs/deleteingDocs.py
@@ -3,3 +3,23 @@ import requests
 from common import Common
 common = Common()
 
+def delete(url, xmlFile, params, headers):
+  data = open(xmlFile, "rb").read()
+  res = requests.post(url, data=data, params=params, headers=headers)
+  print("*"*100)
+  print("Status %s"%res.text)
+  print("*"*100)
+  print()
+
+params = {
+  "commit":"true"
+}
+
+# deleting some documents
+
+delete(common.delete, "data/delete.xml", params, common.headers["xml"])
+
+# deleting all the documents
+
+delete(common.delete, "data/delete-all.xml", params, common.headers["xml"])
+
